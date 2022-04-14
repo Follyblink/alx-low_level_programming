ff296ef98ee760b5dd8b69c69548a9291e3adb56
follyblink <follyblink@gmail.com>
2022-04-14 05:27:59 -0700

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,5 @@
-#include "main.h
-/**
-  _isdigit - function that prints the sign of a number
-  @c: is a digit
-  Return: 1 if c is a digit, 0 otherwise.
-**/
 #include "main.h"
 
-int _isdigit(int c)
 /**
  * _isdigit - checks for a digit (0 through 9)
  * @num: int to be checked
@@ -14,11 +7,5 @@ int _isdigit(int c)
  */
 int _isdigit(int num)
 {
-        if (c >= '0' && c <= '9')
-        {
-                 return (1);
-        }
-    Reiseded with iTop Screen Recorder
-                 return (0);
 	return (num >= '0' && num <= '9');
 }
