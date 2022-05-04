1664444ed0a8a0fdac9997e2365463fd8c30f53d
follyblink <follyblink@gmail.com>
2022-05-04 14:17:33 -0700

    my commits

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @letter: the character to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isupper - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
  */
-int _isupper(int letter)
+
+
+int _isupper(int c)
 {
-	return (letter >= 'A' && letter <= 'Z');
+
+	return (c >= 65 && c <= 90);
 }
--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,17 +1,33 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code for Holberton School students.
- *
- * @c: is an integer param
- *
- * Return: Always 0.
+ * _strncat - two words
+ * @dest : pointer to char param
+ * @src : pointer to char param
+ * @n : int parameter
+ * Return: *dest
  */
 
-
-int _isdigit(int c)
+char *_strncat(char *dest, char *src, int n)
 {
+	int m;
+	int i;
 
-	return (c >= 48 && c <= 57);
+	m = 0;
+
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
 }
-
