bd0d53c07416bcba173f8eab6b8c0cb5a4ce5cfb
follyblink <follyblink@gmail.com>
2022-06-14 05:55:49 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,48 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
- *
- * Return: pointer to the new node, or NULL
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: listint_t
+ * @idx: unsigned int
+ * @n: int
+ * Return: listint_t
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-    listint_t *new;
-    listint_t *temp = *head;
+	listint_t *new = *head;
+	listint_t *temp;
+	unsigned int count = 0;
+	listint_t *node = malloc(sizeof(listint_t));
 
-    new = malloc(sizeof(listint_t));
-    if (!new || !head)
-        return (NULL);
+	if (!node)
+		return (NULL);
 
-    new->n = n;
-    new->next = NULL;
+	node->n = n;
 
-    if (idx == 0)
-    {
-        new->next = *head;
-        *head = new;
-        return (new);
-    }
+	if (((!*head) || !head) && idx != 0)
+		return (NULL);
 
-    for (i = 0; temp && i < idx; i++)
-    {
-        if (i == idx - 1)
-        {
-            new->next = temp->next;
-            temp->next = new;
-            return (new);
-        }
-        else
-            temp = temp->next;
-    }
+	if (((!*head) || (!head)) && idx == 0)
+	{
+		*head = node;
+		return (node);
+	}
 
-    return (NULL);
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
+	for (count = 0; count < idx - 1; count++)
+	{
+		if (new == NULL)
+			return (NULL);
+		new = new->next;
+	}
+		temp = new->next;
+		new->next = node;
+		node->next = temp;
+		return (node);
 }
-
